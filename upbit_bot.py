6e73328aac156a4325a8ce7d1e1dbd037d7e59b6
aiadmin <tjsejrwnd@naver.com>
2020-08-13 09:56:41 +0900

    tabs match

--- upbit_bot.py
+++ upbit_bot.py
@@ -88,7 +88,7 @@ def get_betting_ratio(market):
 
 		if sum_prices/period < prices[0]['opening_price']:
 			score += 1/18.0
-			return score
+	return score
 
 
 def fix_price(price):
@@ -106,7 +106,7 @@ def fix_price(price):
 	for p in _unit:
 		if price > p:
 			price = (price // _unit[p]) * _unit[p]
-			return price
+	return price
 
 def buy(market, budget):
 	for retry in range(3):
@@ -114,15 +114,15 @@ def buy(market, budget):
 		last_price = fix_price(ticker[0]['trade_price'] * (1 + SPREAD_GAP))
 		amount = budget / last_price
 
-	result = upbit.place_order(market, 'bid', amount, last_price)
-	if result and result['uuid']:
-		for i in range(5):
-			order_info = upbit.get_order(result['uuid'])
-			if order_info and float(order_info['remaining_volume']) <= 0.0:
-				return
-	time.sleep(1)
+		result = upbit.place_order(market, 'bid', amount, last_price)
+		if result and result['uuid']:
+			for i in range(5):
+				order_info = upbit.get_order(result['uuid'])
+				if order_info and float(order_info['remaining_volume']) <= 0.0:
+					return
+				time.sleep(1)
 
-	upbit.cancel_order(result['uuid'])
+			upbit.cancel_order(result['uuid'])
 
 
 def sell(market, amount):
@@ -131,21 +131,21 @@ def sell(market, amount):
 		if not ticker:
 			return
 
-	total_price = float(amount) * float(ticker[0]['trade_price'])
-	if total_price < 10000:
-		return
+		total_price = float(amount) * float(ticker[0]['trade_price'])
+		if total_price < 10000:
+			return
 
-	last_price = fix_price(ticker[0]['trade_price'] * (1 - SPREAD_GAP))
-	result = upbit.place_order(market, 'ask', amount, last_price)
+		last_price = fix_price(ticker[0]['trade_price'] * (1 - SPREAD_GAP))
+		result = upbit.place_order(market, 'ask', amount, last_price)
 
-	if result and result['uuid']:
-		for i in range(5):
-			order_info = upbit.get_order(result['uuid'])
-			if order_info and float(order_info['remaining_volume']) <= 0.0:
-				return
-		time.sleep(1)
+		if result and result['uuid']:
+			for i in range(5):
+				order_info = upbit.get_order(result['uuid'])
+				if order_info and float(order_info['remaining_volume']) <= 0.0:
+					return
+				time.sleep(1)
 
-	upbit.cancel_order(result['uuid'])
+			upbit.cancel_order(result['uuid'])
 
 def dump_all():
 	accounts_list = upbit.get_accounts()
@@ -153,16 +153,16 @@ def dump_all():
 	for wallet in accounts_list:
 		sell('KRW-{0}'.format(wallet['currency']), wallet['balance'])
 
-	if __name__ == '__main__':
-		trade_markets = list(candidate_coins())
-		already_buy = {}
-		coin_noise = {}
-		coin_betting_ratio = {}
-		coin_investable = MAX_NUM_COIN
+if __name__ == '__main__':
+	trade_markets = list(candidate_coins())
+	already_buy = {}
+	coin_noise = {}
+	coin_betting_ratio = {}
+	coin_investable = MAX_NUM_COIN
 
 	for market in trade_markets:
 		coin_noise[market] = get_market_noise(market)
-	coin_betting_ratio[market] = get_betting_ratio(market)
+		coin_betting_ratio[market] = get_betting_ratio(market)
 
 	trade_markets = list(filter(lambda m: coin_betting_ratio[m] > 0, trade_markets))
 
@@ -171,29 +171,29 @@ def dump_all():
 			if market in already_buy:
 				continue
 
-		if coin_investable <= 0:
-			break
+			if coin_investable <= 0:
+				break
 
-	candles = upbit.get_candles_daily(market, '', 2)  # Today, Yesterday
+			candles = upbit.get_candles_daily(market, '', 2)  # Today, Yesterday
 
-	_range = candles[1]['high_price'] - candles[1]['low_price']
+			_range = candles[1]['high_price'] - candles[1]['low_price']
 
-	today_opening = candles[0]['opening_price']
-	today_current = candles[0]['trade_price']
+			today_opening = candles[0]['opening_price']
+			today_current = candles[0]['trade_price']
 
-	k = _range * coin_noise[market] * 0.5
+			k = _range * coin_noise[market] * 0.5
 
-	over_ratio = today_current / (today_opening + k)
+			over_ratio = today_current / (today_opening + k)
 
-	if over_ratio > 1.0:
-		buy(market, BETTING_BUDGET * coin_betting_ratio[market])
-		already_buy[market] = True
-		coin_investable -= 1
-		# 만약 현재 시가 기준으로 전날 등락폭 대비해서 올랐으면 사자
+			if over_ratio > 1.0:
+				buy(market, BETTING_BUDGET * coin_betting_ratio[market])
+				already_buy[market] = True
+				coin_investable -= 1
+				# 만약 현재 시가 기준으로 전날 등락폭 대비해서 올랐으면 사자
 
-	time.sleep(1)
+		time.sleep(1)
 
-	t = datetime.now(timezone('Asia/Seoul'))
-	if t.hour == 23 and t.minute > 45:  # 저녁 12시 전에 판매 한다.
-		dump_all()
-exit(0)
+		t = datetime.now(timezone('Asia/Seoul'))
+		if t.hour == 23 and t.minute > 45:  # 저녁 12시 전에 판매 한다.
+			dump_all()
+			exit(0)
